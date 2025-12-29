ec802a3d60e3d59d8a67879a2b5f072bfb6f80ff
moliiner <jamolnav@gmail.com>
2025-12-29 13:49:50 +0100

    Trae changes

---
+++ myWebPage/Pages/Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myWebPage.Pages
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
